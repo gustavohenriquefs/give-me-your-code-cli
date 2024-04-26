72a00a3e782de1f6953f4adcf51100a496218177
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-04-26 09:18:20 -0300

    feat: file test to create db

---
+++ db/index.js
@@ -0,0 +1,13 @@
+// import { db } from '../db.config.js';
+
+// (async () => {
+//     try {
+//         const resultado = await db.siquelize.sync();
+//         console.log(resultado);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
+
+
+// export { db }
