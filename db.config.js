f397751b53dc67bea3aa350fe6513cf9b7262bfe
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-04-27 11:46:34 -0300

    feat: get database path

--- db.config.js
+++ db.config.js
@@ -1,19 +1,24 @@
-import { Sequelize } from 'sequelize'
-import Template from './schema/template.js'
-import { File } from './schema/file.js'
+import { Sequelize } from 'sequelize';
+import { Template } from './schema/template.js';
+import { File } from './schema/file.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './db.sqlite',
+    storage: path.resolve(__dirname, './db.sqlite'),
     logging: false
-})
+});
  
-const db = {}
+const db = {};
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-db.Template = Template({ sequelize, Sequelize })
-db.File = File({ sequelize, Sequelize })
+db.Template = Template({ sequelize, Sequelize });
+db.File = File({ sequelize, Sequelize });
 
 export { db }
