197394f93609b1f436bab68cb33449a9e7b95d54
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-04-27 11:48:14 -0300

    feat: add flags to set template, description

--- index.js
+++ index.js
@@ -2,18 +2,24 @@
 import { program } from 'commander'
 
 import { list } from './commands/list.js'
-import { create } from './commands/create.js'
+import { add } from './commands/add.js'
 import { db } from './db.config.js'
 
-db.sequelize.sync({ force: true })
+db.sequelize.sync()
   .then(() => {});
 
 program.command('list')
         .description('List all templates')
         .action(list)
-
-program.command('create')
-        .description('Create a new template')
-        .action(create)
+        
+program.command('add')
+        .description('Add a new template')
+        .option('-t, --template <templateName>', 'template name')
+        .option('-d, --description <description>', 'description')
+        .option('-f, --file <fileName>', 'file name')
+        .action((options) => {
+                console.log(options)
+                add(options)
+        })
 
 program.parse()
