3c5563a352a73e77aea6b0626530ff8ae82c22ed
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-04-30 03:28:33 -0300

    feat: create command save

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { program } from 'commander'
 
 import { list } from './commands/list.js'
 import { add } from './commands/add.js'
+import { save } from './commands/save.js'
 import { db } from './db.config.js'
 
 db.sequelize.sync()
@@ -18,5 +19,12 @@ program.command('add')
         .option('-d, --description <description>', 'description')
         .option('-f, --file <fileName>', 'file name')
         .action((options) => add(options))
+
+program.command('save')
+        .description('Save a new template')
+        .option('-t, --template <templateName>', 'template name')
+        .option('-d, --description <description>', 'description')
+        .option('-f, --file <fileName>', 'file name')
+        .action((options) => save(options))
 
 program.parse()
